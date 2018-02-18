1b5a34b925cd0fc89eee4f3def51487c9f6d8daf
Kar Joon Chew <chewkj@gmail.com>
2018-02-18 18:07:33 +0800

    parse XML into object

---
+++ READ.md
@@ -0,0 +1,1 @@
+## This is the GWET Connector Project
--- handler.js
+++ handler.js
@@ -1,16 +1,54 @@
 'use strict';
 
-module.exports.createEvent = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+var gwetConnector = require('./lib/gwet-connector');
+var gwetXmlParser = gwetConnector.xmlParser;
+var gwetObjectTransformation = gwetConnector.transfom;
+var sfmcConnector = require('./lib/sfmc-connector');
+var addRowOp = sfmcConnector.addRow;
 
-  callback(null, response);
+async function readFromFS (filename){
+    let filePtr = require ('fs');
+    let util = require('util');
+    const readFileAsync = util.promisify(filePtr.readFile);
+    return await readFileAsync(filename);
+}
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
+
+var filename = './assets/sample-event.xml';
+var gwetObjTemplate = {event: {event_id: '$.Online_Event.Event.Event_ID._text'}};
+var sfmcConfig = require('config').get('country.AU');
+var country = 'AU'
+
+function createEvent(event, context, callback){
+    var addRowToDE = sfmcConnector.connect(sfmcConfig, addRowOp);
+    readFromFS(filename)
+    .then(function (xmlContent) {
+        let parseObj = gwetConnector.parse(xmlContent)
+                                           (gwetXmlParser)
+                                           (gwetObjectTransformation)
+                                           (gwetObjTemplate);
+        let addResult = addRowToDE(parseObj);
+        const response = {
+          statusCode: 200,
+          body: JSON.stringify({
+            message: `parseObj: ${JSON.stringify(parseObj)}, addResult: ${addResult}`,
+            input: event,
+          })
+        };
+        callback(null, response);    
+    });
+}
+
+//Test Run
+let event = {};
+let context = {};
+function callback (err, response){
+  if (err){
+    throw new Error (err);
+  }
+  console.log(JSON.stringify(response));
+}
+createEvent(event, context, callback);
+
+
+module.exports = { createEvent }
